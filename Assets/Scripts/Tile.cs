1a58f13154f45099a3f4ead85ddc7089971f530f
Andreas Holzner <andiamant4427@gmail.com>
2022-12-10 15:29:44 +0100

    added prefabs and materials

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
+[CanEditMultipleObjects]
 public class Tile : MonoBehaviour
 {
+    
+    [SerializeField]
+    public GameObject[] prefabs = new GameObject[4];
     public enum TileType{
-        GRASS,
-        WOOD,
-        WATER
+        DEFAULT = 0,
+        GRASS = 1,
+        WOOD = 2,
+        WATER = 3
     }
+    
     public TileType type;
+
+
+
+
+    public void ChangeTile(){
+        GameObject newTile = PrefabUtility.InstantiatePrefab(prefabs[(int)type]) as GameObject;
+        newTile.transform.position = this.transform.position;
+        newTile.transform.parent = this.transform.parent;
+        DestroyImmediate(gameObject);
+    }
 }
