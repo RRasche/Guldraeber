694f504425db63b9490a00a341e216e4f9e97b8f
Andreas Holzner <andiamant4427@gmail.com>
2022-12-10 12:40:39 +0100

    added Map Generator

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapGenerator : MonoBehaviour
+{
+    public GameObject hexPrefab;
+    public const float outerRadius = 1.0f;
+    public const float innerRadius = outerRadius * 0.866025404f;
+    public int width = 50;
+    public int height = 31;
+
+    public Vector3 startingLocation = new Vector3(0,0,0);
+
+    public Tile[][] map;
+
+    public void GenerateMap(){
+        map = new Tile[height][];
+        for(int i = 0; i < height; i++){
+            map[i] = new Tile[width - i % 2];
+        }
+        GameObject mapObj = new GameObject();
+        mapObj.name = "TileMap";
+
+        float x = - ((float) width - 1) / 2 * innerRadius;
+        float z = - ((float) height - 1) / 2 * outerRadius;
+
+        for(int i = 0; i < map.Length; i++){
+            float h = startingLocation.y - ((float) height - 1) / 2 * outerRadius + i * outerRadius * 1.5f;
+            for(int j = 0; j < map[i].Length; j++){
+                float w = startingLocation.x - ((float) width - 1) / 2 * innerRadius + innerRadius * (i % 2) + j * 2 * innerRadius;
+                GameObject tile = PrefabUtility.InstantiatePrefab(hexPrefab) as GameObject;
+                tile.transform.position = new Vector3(w, h, 0.5f);
+                tile.transform.parent = mapObj.transform;
+            }
+        }
+
+    }
+
+
+}
