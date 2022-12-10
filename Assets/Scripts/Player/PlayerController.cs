df2f14ee854919e77b65070db09e3baf54c16465
nav0t <realandnotfake@email.com>
2022-12-10 16:06:23 +0100

    decent movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,22 +5,37 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private float acceleration = 1;
+    [SerializeField] private float turnAcceleration = 1;
+
 
     private Vector2 _moveDir;
+    private Rigidbody2D rigidbody2D;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 moveDir = this._moveDir;
-        //Debug.Log(moveDir);
-        transform.position += new Vector3(moveDir.x, moveDir.y, 0);
+
+        Vector2 oldVel = rigidbody2D.velocity;
+        Vector2 targetVel = moveDir * speed;
+
+        Vector2 vel = Vector2.Lerp(oldVel, targetVel, acceleration * Time.deltaTime);
+
+        rigidbody2D.velocity = vel;
         if (moveDir.sqrMagnitude > 0) {
-            transform.forward = new Vector3(moveDir.x, moveDir.y, 0).normalized;
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, turnAcceleration * Time.deltaTime);
+            transform.forward = new Vector3(transform.forward.x, transform.forward.y, 0);
         }
     }
 
     public void ChangeMoveDir(Vector2 moveDir) {
-        this._moveDir = moveDir * Time.deltaTime * this.speed;
+        this._moveDir = moveDir;
     }
 }
